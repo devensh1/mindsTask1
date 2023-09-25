749707194b2ecf5fd1a5433af4b10a7a3d5eab1d
devensh1 <devenshpal@gmail.com>
2023-09-25 16:28:14 +0530

    third

--- app.js
+++ app.js
@@ -24,47 +24,53 @@ if (elements) {
 }
 
 // Get references to the table and the button
-var table = document.querySelector(".table");
-var addRowButton = document.getElementById("addRowButton");
-var deleteRowButton = document.getElementById("delete-row-button");
+//$ is for fetching data directly//
+$(document).ready(function () {
+  var table = $(".table");
+  var addRowButton = $("#addRowButton");
+  var deleteRowButton = $("#delete-row-button");
 
-if (addRowButton) {
-  addRowButton.addEventListener("click", function () {
-    // Create a new row and cells
-    var newRow = table.insertRow(-1); // -1 inserts the row at the end of the table
-    for (var i = 0; i < 17; i++) {
-      // Create 17 cells for each row
-      var cell = newRow.insertCell(i);
-      var input = document.createElement("input");
-      input.style.border = "none";
-      input.style.resize = "none";
-      input.style.height= "1vh";
-      input.style.width = "5vw";
-      input.style.padding = "-10px";
-     
-      cell.appendChild(input);
-      if (i === 15) {
-        // For the "Status" column, create a select dropdown
-        var select = document.createElement("select");
-        select.innerHTML =
-          '<option value="volvo">Active</option><option value="saab">Not Active</option><option value="mercedes">Pending</option>';
-        cell.appendChild(select);
-      }
-    }
-  });
-}
+  if (addRowButton.length) {
+    addRowButton.on("click", function () {
+      // Create a new row and cells
+      var newRow = $("<tr></tr>");
+      for (var i = 0; i < 17; i++) {
+        // Create 17 cells for each row
+        var cell = $("<td></td>");
+        var input = $("<input>").css({
+          border: "none",
+          resize: "none",
+          height: "1vh",
+          width: "5vw",
+          padding: "-10px",
+        });
 
-
-
-
-if (deleteRowButton) {
-    deleteRowButton.addEventListener("click", function () {
-        var rowCount = table.rows.length;
-        if (rowCount > 0) {
-            table.deleteRow(rowCount - 1); // Delete the last row
+        cell.append(input);
+        if (i === 15) {
+          // For the "Status" column, create a select dropdown
+          var select = $("<select></select>").html(
+            '<option value="volvo">Active</option><option value="saab">Not Active</option><option value="mercedes">Pending</option>'
+          );
+          cell.append(select);
         }
+        newRow.append(cell);
+      }
+      table.append(newRow);
     });
-}
+  }
+
+  if (deleteRowButton.length) {
+    deleteRowButton.on("click", function () {
+      var rowCount = table.find("tr").length;
+      if (rowCount > 0) {
+        table.find("tr:last").remove(); // Delete the last row
+      }
+    });
+  }
+});
+
+
+
 
 
 
